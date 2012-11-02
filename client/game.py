593050c619f8549fbc06b091f06ffe7cf4c51fc4
MJ-meo-dmt <francois.debruyn66@gmail.com>
2012-11-02 09:50:55 +0200

    Basic login and move to menu works now

--- client/game.py
+++ client/game.py
@@ -40,7 +40,7 @@ logging.basicConfig(filename=LOGDIR+'client.log',level=logging.DEBUG,
 #  logging.debug('This message should go to the log file')
 #  logging.info('So should this')
 #  logging.warning('And this, too')
-#
+##
 #################################
 
 ## IMPORTS ##
